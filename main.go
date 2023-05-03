c52d1c86af2bfcd937d861977915abc1332fe3a1
Rodrigo Gomes <rodrigo.lud@gmail.com>
2023-05-03 11:15:22 -0700

    More samples

--- main.go
+++ main.go
@@ -2,104 +2,46 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 //"math/rand"
 
 func main() {
 
-	// *********** gess a number block ***********
+	guessNumber()  // Gess a number between 1 and 100
+	ifSample()     // Demonstrating the use of if, else and else if
+	switchSample() // Switch without switch statement
+	logicSample()  // Logical Operators
+}
 
-	// n := rand.Intn(101)
-	// println(n)
-	// fmt.Printf("Please guess a number between 1 and 100 ")
+// *********** gess a number block ***********
+func guessNumber() {
 
-	// for {
-	// 	var guess int
-	// 	fmt.Scanf("%d", &guess)
-	// 	if guess == n {
-	// 		fmt.Println("\nYou've guessed right!")
-	// 		break
-	// 	}
+	n := rand.Intn(101)
+	println(n)
+	fmt.Printf("Please guess a number between 1 and 100 ")
 
-	// 	fmt.Print("Your guess is incorrect. ")
-	// 	if guess > n {
-	// 		fmt.Print("Try again < :")
-	// 	} else {
-	// 		fmt.Print("Try again > :")
-	// 	}
-	// }
+	for {
+		var guess int
+		fmt.Scanf("%d", &guess)
+		if guess == n {
+			fmt.Println("\nYou've guessed right!")
+			break
+		}
 
-	// ***********  print numbers - 1 to 100 ***********
+		fmt.Print("Your guess is incorrect. ")
+		if guess > n {
+			fmt.Print("Try again < :")
+		} else {
+			fmt.Print("Try again > :")
+		}
+	}
 
-	// islast := false
-	// for i := 1; i <= 100; i++ {
-	// 	if i == 100 {
-	// 		islast = true
-	// 	}
-	// 	if !islast {
-	// 		fmt.Print(i, ", ")
-	// 	} else {
-	// 		fmt.Print(i)
-	// 	}
-	// }
+}
 
-	// ***********  All capital letters unicode code ***********
-
-	// for i := 65; i <= 90; i++ { //A-Z (A = 65, B = 66,...)
-	// 	fmt.Printf("\t%#U", i)
-	// 	fmt.Println(" - ", i)
-	// }
-
-	// ***********  My birthday year until now and my age ***********
-
-	// myYear := 1978 // enter the year of your birthday here
-	// year := time.Now().Year()
-	// countAge := -1
-	// for i := myYear; i <= year; i++ {
-	// 	countAge++
-	// 	fmt.Print(i, ", ")
-	// }
-	// fmt.Println("My age is:", countAge)
-	// fmt.Println("My age is:", year-myYear)
-
-	// ***********  My birthday year until now and my age 2***********
-
-	// myYear := 1978 // enter the year of your birthday here
-	// countAge := -1
-	// for myYear <= time.Now().Year() {
-	// 	countAge++
-	// 	fmt.Print(myYear, "\t")
-	// 	myYear++
-	// }
-	// fmt.Println("\nMy age is:", countAge)
-
-	// ***********  My birthday year until now and my age 3 ***********
-
-	// myYear := 1978 // enter the year of your birthday here
-	// countAge := -1
-	// for {
-	// 	fmt.Print(myYear, "\t")
-	// 	countAge++
-	// 	myYear++
-	// 	if myYear > time.Now().Year() {
-	// 		break
-	// 	}
-	// }
-	// fmt.Println("\nMy age is:", countAge)
-
-	// ***********  Remainder after division (modulo function) of numbers between 10 and 100 ***********
-	// for i := 10; i <= 100; i++ {
-	// 	fmt.Print(math.Mod(float64(i), 4), "\t")
-	// }
-
-	// ***********  Remainder after division (modulo function) of numbers between 10 and 100 - 2 ***********
-	// for i := 10; i <= 100; i++ {
-	// 	fmt.Print((i % 4), "\t")
-	// }
-
-	// ***********  Demonstrating the use of if, else and else if ***********
-
+// ***********  Demonstrating the use of if, else and else if ***********
+func ifSample() {
 	isTheDayBeauty := false
 	if isTheDayBeauty {
 		fmt.Println("Let's to the beach!")
@@ -115,9 +57,10 @@ func main() {
 	} else {
 		fmt.Println("The sofa fit well in your living room")
 	}
+}
 
-	// ***********  Switch without switch statement ***********
-
+// ***********  Switch without switch statement ***********
+func switchSample() {
 	favoriteSport := "Soccer"
 	switch favoriteSport {
 	case "Basketball":
@@ -125,7 +68,83 @@ func main() {
 	case "Volleyball":
 		fmt.Println("Volleyball is in Italy")
 	case "Soccer":
-		fmt.Println("Soccer is in Brazil") // default case, if no other case matches, it will execute.  If you don't
+		fmt.Println("Soccer is in Brazil")
 	}
-
 }
+
+// ***********  Logical Operators ***********
+func logicSample() {
+	//fmt.Println(true && true)
+	//fmt.Println(true || true)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
+
+// ***********  print numbers - 1 to 100 ***********
+
+// islast := false
+// for i := 1; i <= 100; i++ {
+// 	if i == 100 {
+// 		islast = true
+// 	}
+// 	if !islast {
+// 		fmt.Print(i, ", ")
+// 	} else {
+// 		fmt.Print(i)
+// 	}
+// }
+
+// ***********  All capital letters unicode code ***********
+
+// for i := 65; i <= 90; i++ { //A-Z (A = 65, B = 66,...)
+// 	fmt.Printf("\t%#U", i)
+// 	fmt.Println(" - ", i)
+// }
+
+// ***********  My birthday year until now and my age ***********
+
+// myYear := 1978 // enter the year of your birthday here
+// year := time.Now().Year()
+// countAge := -1
+// for i := myYear; i <= year; i++ {
+// 	countAge++
+// 	fmt.Print(i, ", ")
+// }
+// fmt.Println("My age is:", countAge)
+// fmt.Println("My age is:", year-myYear)
+
+// ***********  My birthday year until now and my age 2***********
+
+// myYear := 1978 // enter the year of your birthday here
+// countAge := -1
+// for myYear <= time.Now().Year() {
+// 	countAge++
+// 	fmt.Print(myYear, "\t")
+// 	myYear++
+// }
+// fmt.Println("\nMy age is:", countAge)
+
+// ***********  My birthday year until now and my age 3 ***********
+
+// myYear := 1978 // enter the year of your birthday here
+// countAge := -1
+// for {
+// 	fmt.Print(myYear, "\t")
+// 	countAge++
+// 	myYear++
+// 	if myYear > time.Now().Year() {
+// 		break
+// 	}
+// }
+// fmt.Println("\nMy age is:", countAge)
+
+// ***********  Remainder after division (modulo function) of numbers between 10 and 100 ***********
+// for i := 10; i <= 100; i++ {
+// 	fmt.Print(math.Mod(float64(i), 4), "\t")
+// }
+
+// ***********  Remainder after division (modulo function) of numbers between 10 and 100 - 2 ***********
+// for i := 10; i <= 100; i++ {
+// 	fmt.Print((i % 4), "\t")
+// }
